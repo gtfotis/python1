4e2ae5e75135458a74c9e3b35926ecf975d76b9d
otis wilcox <otiswilcox@gmail.com>
2021-05-05 16:12:55 -0400

    edited some things

--- madlib1.py
+++ madlib1.py
@@ -1,10 +1,10 @@
 print("Please fill in the blanks below:")
-print("_(name)_'s favorite video game is _(subject)_.")
+print("_(name)_'s favorite game is _(subject)_.")
 user_name = input("What is your name? ")
 favorite_game = input("What is your favorite game? ")
 length_of_name = len(user_name)
 length_of_game = len(favorite_game)
-if length_of_name > 0 and length_of_game >0:
+if length_of_name > 0 and length_of_game > 0:
     print("%s's favorite game is %s." % (user_name, favorite_game))
 else:
     print("Well that was rude, I'll ask later I guess :(")
