524c7b95e6cc0c9cbef1677c0a6884301d8ba9c4
otis wilcox <otiswilcox@gmail.com>
2021-05-06 11:21:20 -0400

    removed unnecessary arithmetic

--- work_or_sleep_in.py
+++ work_or_sleep_in.py
@@ -1,7 +1,7 @@
 day = int(input('Day (1-7)? '))
-if day-1 == 0 or day-1 == 6:
+if day == 1 or day == 7:
     print("Sleep in")
-elif day-1 == 1 or day-1 == 2 or day-1 == 3 or day-1 == 4 or day-1 == 5:
+elif day == 2 or day == 3 or day == 4 or day == 5 or day == 6:
     print("Go to work")
 else:
     print("How many days are in a week again?")
