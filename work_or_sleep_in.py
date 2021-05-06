773be78967bcd18d900bfc767c7e6f1df2b477bb
otis wilcox <otiswilcox@gmail.com>
2021-05-05 23:24:22 -0400

    added work or sleep in python 'calculator' (day 3 hw)

---
+++ work_or_sleep_in.py
@@ -0,0 +1,17 @@
+day = int(input('Day (1-7)? '))
+if day-1 == 0:
+    print("Sleep in")
+elif day-1 == 1:
+    print("Go to work")
+elif day-1 == 2:
+    print("Go to work")
+elif day-1 == 3:
+    print("Go to work")
+elif day-1 == 4:
+    print("Go to work")
+elif day-1 == 5:
+    print("Go to work")
+elif day-1 == 6:
+    print("Sleep in")
+else:
+    print("How many days are in a week again?")
