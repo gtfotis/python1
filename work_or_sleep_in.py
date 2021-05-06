fe470883f0f8d473e1b61e42c78f7ae870c0f204
otis wilcox <otiswilcox@gmail.com>
2021-05-06 10:44:11 -0400

    shortened the code to make it look cleaner

--- work_or_sleep_in.py
+++ work_or_sleep_in.py
@@ -1,17 +1,7 @@
 day = int(input('Day (1-7)? '))
-if day-1 == 0:
+if day-1 == 0 or day-1 == 6:
     print("Sleep in")
-elif day-1 == 1:
+elif day-1 == 1 or day-1 == 2 or day-1 == 3 or day-1 == 4 or day-1 == 5:
     print("Go to work")
-elif day-1 == 2:
-    print("Go to work")
-elif day-1 == 3:
-    print("Go to work")
-elif day-1 == 4:
-    print("Go to work")
-elif day-1 == 5:
-    print("Go to work")
-elif day-1 == 6:
-    print("Sleep in")
 else:
     print("How many days are in a week again?")
